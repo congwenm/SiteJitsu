8c96545e5ecc27c1c0867bf6057e53f5ce99d6d6
Congwen Ma <lostaznfound@gmail.com>
2013-12-07 03:24:23 -0600

    yanfeis store

---
+++ WebStore/app/scripts/controllers.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// angular.module('WebStoreApp')
+  app.controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+
+app.controller('HomeCtrl', function($scope){
+
+
+
+})
+
+.controller('LoginCtrl', function($scope, $rootScope, LoginService){
+	$scope.login = function(e){
+		console.log($scope);
+		$scope.loginForm.submitten = true;
+		e.stopPropagation();
+		
+		$(window).click(function(){
+			$rootScope.$apply(function(){
+				$scope.loginForm.submitten = false;
+			})
+		})
+	
+
+		//login on valid form
+		if($scope.loginForm.$valid){
+			LoginService.get({
+				'user_email': $scope.loginForm.email.$viewValue,
+				'user_password': $scope.loginForm.password.$viewValue
+			}, function(response){
+				console.log('logged in??', response);
+				$rootScope.LoginResponse = response;
+			})// send in username and password
+		}
+		else{
+			//do not log in
+			if ($scope.loginForm.email.$invalid){
+				$scope.loginMsg = "Please Check your email";
+			}
+			else if ($scope.loginForm.password.$invalid){
+				$scope.loginMsg = "Please Check your password";
+			}
+		}
+	}
+})
